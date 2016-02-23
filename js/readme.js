b437db532a757b2c4d9c4f1fd4abd0d5b0485318
Tulsian, Rishabh (GE Global Research) <Rishabh.Tulsian@ge.com>
2016-02-22 19:34:16 -0800

    Get images from repo

--- js/readme.js
+++ js/readme.js
@@ -1,71 +1,73 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	var URLs = {},
-		REPO;
+  var URLs = {},
+  REPO;
 
-	REPO = getRepo();
+  REPO = getRepo();
   URLs.readme = 'https://api.github.com/repos/' + REPO + '/contents/README.md';
   URLs.markdown = 'https://api.github.com/markdown';
 
-	getResource( URLs.readme, onReadme );
+  getResource( URLs.readme, onReadme );
 
-	// FUNCTIONS //
+  // FUNCTIONS //
 
   function getRepo() {
     return document.getElementById( 'readme' ).getAttribute( 'repo' );
   }
 
   function onReadme( blob ) {
-		var content = window.atob( JSON.parse( blob ).content );
-		render( content );
-	}
+    var content = window.atob( JSON.parse( blob ).content );
+    render( content );
+  }
 
   function onResource( html ) {
-		document.getElementById( 'readme' ).innerHTML = html;
-	}
+    var search = new RegExp('src="images/', 'g');
+    html = html.replace(search, 'src="https://raw.githubusercontent.com/' + REPO + '/master/images/');
+    document.getElementById( 'readme' ).innerHTML = html;
+  }
 
-	function getResource( url, clbk ) {
-		var xhr;
-		if ( url && clbk ) {
-			xhr = new XMLHttpRequest();
-			xhr.open( 'GET', url );
+  function getResource( url, clbk ) {
+    var xhr;
+    if ( url && clbk ) {
+      xhr = new XMLHttpRequest();
+      xhr.open( 'GET', url );
 
-			xhr.onreadystatechange = function () {
-				if ( xhr.readyState != 4 || xhr.status != 200 ){
-					return;
-				}
-				clbk( xhr.responseText );
-			};
-			xhr.send();
-		}
-	}
+      xhr.onreadystatechange = function () {
+        if ( xhr.readyState != 4 || xhr.status != 200 ){
+          return;
+        }
+        clbk( xhr.responseText );
+      };
+      xhr.send();
+    }
+  }
 
-	function postResource( url, data, clbk ) {
-		var xhr;
-		if ( url && clbk ) {
-			xhr = new XMLHttpRequest();
-			xhr.open( 'POST', url, true );
+  function postResource( url, data, clbk ) {
+    var xhr;
+    if ( url && clbk ) {
+      xhr = new XMLHttpRequest();
+      xhr.open( 'POST', url, true );
 
-			xhr.setRequestHeader( 'Content-Type', 'application/json' );
+      xhr.setRequestHeader( 'Content-Type', 'application/json' );
 
-			xhr.onreadystatechange = function () {
-				if ( xhr.readyState != 4 || xhr.status != 200 ){
-					return;
-				}
-				clbk( xhr.responseText );
-			};
-			xhr.send( data );
-		}
-	}
+      xhr.onreadystatechange = function () {
+        if ( xhr.readyState != 4 || xhr.status != 200 ){
+          return;
+        }
+        clbk( xhr.responseText );
+      };
+      xhr.send( data );
+    }
+  }
 
-	function render( content ) {
-		content = {
-			'text': content,
-			'mode': 'markdown',
-			'context': REPO
-		};
-		content = JSON.stringify( content );
-		postResource( URLs.markdown, content, onResource );
-	}
+  function render( content ) {
+    content = {
+      'text': content,
+      'mode': 'markdown',
+      'context': REPO
+    };
+    content = JSON.stringify( content );
+    postResource( URLs.markdown, content, onResource );
+  }
 })();
